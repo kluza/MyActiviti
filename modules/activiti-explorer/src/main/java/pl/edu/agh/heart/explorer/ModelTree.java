8c128cfe23b652b0599c61828c4c7899dec7004e
Roman <roman-himself@o2.pl>
2013-07-24 20:14:14 +0200

    minor change in error messages

--- modules/activiti-explorer/src/main/java/pl/edu/agh/heart/explorer/ModelTree.java
+++ modules/activiti-explorer/src/main/java/pl/edu/agh/heart/explorer/ModelTree.java
@@ -40,15 +40,16 @@ public class ModelTree {
             }
             tree.addListener(new ModelTreeListener(tree, panel));
         } catch (IOException exception) {
-            String errMsg = "Couldn't connect";
+            String errMsg = "Communication exception";
             tree.addItem(errMsg);
             tree.setChildrenAllowed(errMsg, false);
         } catch (Exception exception) {
-            String errMsg = "Something's fucked up";
+            String errMsg = "HeaRT connection failed!";
             tree.addItem(errMsg);
             tree.setChildrenAllowed(errMsg, false);
+        } finally {
+            return tree;
         }
-        return tree;
     }
     
     static class Model {
